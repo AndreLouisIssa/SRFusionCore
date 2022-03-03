b55c6e876228bb1035d0dfc6727fc8c9b0a763b9
Andre Issa <andrelouisissa@gmail.com>
2022-03-04 01:36:27 +1100

    parameter helpers

--- Parameter.cs
+++ Parameter.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace FusionCore
+﻿namespace FusionCore
 {
     public class Parameter
     {
@@ -11,6 +7,14 @@ namespace FusionCore
 
         public T GetValue<T>() => (T)value;
 
+        public bool TryGetValue<T>(out T value)
+        {
+            value = default;
+            if (!typeof(T).IsAssignableFrom(this.value.GetType()))
+                return false;
+            value = (T)this.value;
+            return true;
+        }
         public Parameter(Form form, object value)
         {
             this.form = form; this.value = value;
@@ -19,6 +23,20 @@ namespace FusionCore
         public static Parameter Parse(Form form, string value)
         {
             return new Parameter(form, form.read(value));
+        }
+
+        public static bool TryParse(Form form, string value, out Parameter param)
+        {
+            param = null;
+            if (!form.check(value))
+                return false;
+            param = Parse(form, value);
+            return true;
+        }
+
+        public Form GetForm()
+        {
+            return form.Is(value);
         }
 
         public override string ToString()
