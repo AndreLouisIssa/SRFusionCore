df235e839439398fda5d76cbf9eb280a047faf6c
Andre Issa <andrelouisissa@gmail.com>
2022-03-04 01:40:13 +1100

    remove form names

--- Form.cs
+++ Form.cs
@@ -9,23 +9,20 @@ namespace FusionCore
     // @MagicGonads
     public class Form
     {
-        public readonly string name;
         public readonly Type type;
         public readonly bool isEnum;
         public readonly string hint;
         public readonly Func<string, List<string>> auto;
-        public readonly Func<string, object> read;
-        public readonly Func<string, bool> check = (v) => true;
-        public readonly Func<object, string> show = (v) => v.ToString().ToLower();
+        public readonly Func<string, object> read = (s) => s;
+        public readonly Func<string, bool> check = (s) => true;
+        public readonly Func<object, string> show = (v) => v.ToString();
             
 
-        public Form(string name, Type type, bool isEnum, string hint, Func<string, List<string>> auto,
+        public Form(Type type, bool isEnum, string hint, Func<string, List<string>> auto,
             Func<string, object> read, Func<string, bool> check = null, Func<object, string> show = null)
         {
             this.type = type; this.isEnum = isEnum; this.hint = hint; this.auto = auto;
             this.read = read; this.check = check ?? this.check; this.show = show ?? this.show;
-            this.name = name; if (!instances.ContainsKey(name)) instances.Add(name, this);
-            else Log.Warning($"{nameof(FusionCore)}: {nameof(Form)} by name \"{name}\" already exists, will be ignored by commands.");
         }
 
         public virtual Form Is(string s)
@@ -44,7 +41,7 @@ namespace FusionCore
             public readonly Form to;
 
             public Join(Form from, Form to) : base(
-                from.name + "|" + to.name, typeof(object), from.isEnum && to.isEnum, from.hint + "|" + to.hint, s => from.auto(s).Union(to.auto(s)).ToList(),
+                typeof(object), from.isEnum && to.isEnum, from.hint + "|" + to.hint, s => from.auto(s).Union(to.auto(s)).ToList(),
                 s => (from.Is(s) ?? to.Is(s)).read(s), s => from.check(s) || to.check(s), o => (from.Is(o) ?? to.Is(o)).show(o)
             )
             { this.from = from; this.to = to; }
@@ -68,17 +65,17 @@ namespace FusionCore
 
         public class Singleton<T> : Form
         {
-            public readonly string key;
+            public readonly string name;
             public readonly T value;
 
-            public Singleton(string key, T value) : base(
-                key, typeof(T), false, $"'{key}'", s => new List<string>{ key },
-                s => value, s => s == key, o => key
-            ) { this.key = key; this.value = value; }
+            public Singleton(string name, T value) : base(
+                typeof(T), false, $"'{name}'", s => new List<string>{ name },
+                s => value, s => s == name, o => name
+            ) { this.name = name; this.value = value; }
 
             public override Form Is(string s)
             {
-                return (s == key) ? this : null;
+                return (s == name) ? this : null;
             }
 
             public override Form Is(object o)
@@ -104,21 +101,21 @@ namespace FusionCore
 
             public static Form Null = new Singleton<object>("null", null);
 
-            public static Form Bool = new Form("Bool", typeof(bool), false, "true|false", s => new List<string> { true.ToString(), false.ToString() }, s => bool.Parse(s), s => bool.TryParse(s, out _));
-            public static Form Int = new Form("Int", typeof(int), false, "integer", s => new List<string> { default(int).ToString() }, s => int.Parse(s), s => int.TryParse(s, out _));
-            public static Form Float = new Form("Float", typeof(float), false, "float", s => new List<string> { default(float).ToString() }, s => float.Parse(s), s => float.TryParse(s, out _));
-            public static Form Double = new Form("Double", typeof(double), false, "double", s => new List<string> { default(double).ToString() }, s => double.Parse(s), s => double.TryParse(s, out _));
-            public static Form String = new Form("String", typeof(string), false, "string", s => new List<string> { }, s => s);
+            public static Form Bool = new Form(typeof(bool), false, "true|false", s => new List<string> { true.ToString(), false.ToString() }, s => bool.Parse(s), s => bool.TryParse(s, out _));
+            public static Form Int = new Form(typeof(int), false, "integer", s => new List<string> { default(int).ToString() }, s => int.Parse(s), s => int.TryParse(s, out _));
+            public static Form Float = new Form(typeof(float), false, "float", s => new List<string> { default(float).ToString() }, s => float.Parse(s), s => float.TryParse(s, out _));
+            public static Form Double = new Form(typeof(double), false, "double", s => new List<string> { default(double).ToString() }, s => double.Parse(s), s => double.TryParse(s, out _));
+            public static Form String = new Form(typeof(string), false, "string", s => new List<string> { }, s => s);
 
-            public static Form Mode = new Form("Mode", typeof(Mode), false, "mode",
+            public static Form Mode = new Form(typeof(Mode), false, "mode",
                 s => Core.fusionModes.Keys.ToList(),
                 s => Core.fusionModes[s], s => Core.fusionModes.ContainsKey(s), o => ((Mode)o).Blame);
 
-            public static Form Slime = new Form("Slime", typeof(SlimeDefinition), true, "'PINK_SLIME' etc.",
+            public static Form Slime = new Form(typeof(SlimeDefinition), true, "'PINK_SLIME' etc.",
                 s => GameContext.Instance.SlimeDefinitions.Slimes.Select(Core.GetFullName).ToList(),
                 s => Core.GetSlimeByFullName(s),s => GameContext.Instance.SlimeDefinitions.Slimes.Any(d => d.GetFullName() == s),
                 s => ((SlimeDefinition)s).GetFullName());
-            public static Form SlimePair = new Form("SlimePair", typeof(List<SlimeDefinition>), true, "'PINK_SLIME-GOLD_SLIME' etc.",
+            public static Form SlimePair = new Form(typeof(List<SlimeDefinition>), true, "'PINK_SLIME-GOLD_SLIME' etc.",
                 s =>
                 {
                     var chosen = s.Substring(0, s.IndexOf('-') + 1).ToUpper();
@@ -130,7 +127,7 @@ namespace FusionCore
                 {
                     return string.Join("-", ((List<SlimeDefinition>)l).Select(Core.GetFullName));
                 });
-            public static Form Slimes = new Form("Slimes", typeof(List<SlimeDefinition>), false, "'PINK_SLIME-GOLD_SLIME-SABER_SLIME' etc.",
+            public static Form Slimes = new Form(typeof(List<SlimeDefinition>), false, "'PINK_SLIME-GOLD_SLIME-SABER_SLIME' etc.",
                 s =>
                 {
                     var chosen = s.Substring(0, s.LastIndexOf('-') + 1).ToUpper();
@@ -143,9 +140,9 @@ namespace FusionCore
                     return string.Join("-", ((List<SlimeDefinition>)l).Select(Core.GetFullName));
                 });
 
-            public static Form PureSlime = new Form("PureSlime", typeof(SlimeDefinition), false, "'PINK' etc.",
+            public static Form PureSlime = new Form(typeof(SlimeDefinition), false, "'PINK' etc.",
                 s => Core.pureSlimes.Keys.ToList(), s => Core.pureSlimes[s], s => true, s => Core.PureName(((SlimeDefinition)s).GetFullName()));
-            public static Form PureSlimePair = new Form("PureSlimePair", typeof(List<SlimeDefinition>), false, "'PINK-GOLD' etc.",
+            public static Form PureSlimePair = new Form(typeof(List<SlimeDefinition>), false, "'PINK-GOLD' etc.",
                 s =>
                 {
                     var chosen = s.Substring(0, s.IndexOf('-') + 1).ToUpper();
@@ -157,7 +154,7 @@ namespace FusionCore
                 {
                     return string.Join("-", ((List<SlimeDefinition>)l).Select(Core.GetFullName).Select(Core.PureName));
                 });
-            public static Form PureSlimes = new Form("PureSlimes", typeof(List<SlimeDefinition>), false, "'PINK-GOLD-SABER' etc.",
+            public static Form PureSlimes = new Form(typeof(List<SlimeDefinition>), false, "'PINK-GOLD-SABER' etc.",
                 s =>
                 {
                     var chosen = s.Substring(0, s.LastIndexOf('-') + 1).ToUpper();
